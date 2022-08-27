38ca3bdabac8b27579de67044ebc148685ed5488
Aishwariya <76518957+Aishwariya02@users.noreply.github.com>
2022-08-27 19:54:51 +0530

    Update GaussSeidel.py

--- Worksheet1/GaussSeidel.py
+++ Worksheet1/GaussSeidel.py
@@ -1,13 +1,24 @@
 import re 
+
+def isDiagonalDominant(a):
+    for i in range(len(a)):
+        sum = 0
+        for j in range(len(a[0])):
+            if i!= j:
+                sum += a[i][j]
+        if a[i][i] >= sum:
+            continue
+        else:
+            return False
+    return True
+
 n = int(input("Enter the number of equations: "))
-eq = []
+eqns = []
 for i in range(n):
     s = input("Enter equation " + str(i+1) + ":")
-    eq.append(s)
-
+    eqns.append(s)
 a = []
-
-for x in eq:
+for x in eqns:
     x = re.findall("(-?[\d]+)", x)
     x = [int(i) for i in x]
     a.append(x)
@@ -16,18 +27,20 @@ print("Matrix form: ")
 for i in range(len(a)):
     print(a[i])
 
-i = 0
-initail  = [0]*n
-values = [0]*n
-maxiter = 25
-while i < maxiter:
-    for j in range(len(a)):
-        k =0
-        values[j] = a[j][-1]
-        while k < (len(a[j])-1):
-            if k!=j:
-                values[j] -= values[k]*a[j][k]
-            k+=1      
-        values[j] /= a[j][j];
-    print(values)
-    i+=1;
+
+if(isDiagonalDominant(a)):
+    i = 0
+    initail  = [0]*n
+    values = [0]*n
+    maxiter = 25
+    while i < maxiter:
+        for j in range(len(a)):
+            k =0
+            values[j] = a[j][-1]
+            while k < (len(a[j])-1):
+                if k!=j:
+                    values[j] -= values[k]*a[j][k]
+                k+=1      
+            values[j] /= a[j][j];
+        print(values)
+        i+=1;
