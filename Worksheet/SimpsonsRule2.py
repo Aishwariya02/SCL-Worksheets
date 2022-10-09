c7493c6c4cd0e25f882ed3caaa02a7e264107c35
Aishwariya <76518957+Aishwariya02@users.noreply.github.com>
2022-10-09 20:59:57 +0530

    Create SimpsonsRule2.py

---
+++ Worksheet/SimpsonsRule2.py
@@ -0,0 +1,13 @@
+import math
+def simpson(f, a, b, n):
+    h = (b - a) / n
+    s = f(a) + f(b)
+
+    for i in range(1, n, 2):
+        s += 4 * f(a + i * h)
+    for i in range(2, n-1, 2):
+        s += 2 * f(a + i * h)
+
+    return s * h / 3
+
+print(simpson(lambda x:math.e**(-(x**2)), 0, 1, 100))
